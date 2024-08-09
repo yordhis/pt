b14dd521dd7c011191529f10e3ca02e335483320
yordhis <yordhis.10@gmail.com>
2024-08-09 15:58:17 -0400

    Se validdo el valor de modo dev a modo prod de la variable de entorno recibida

--- src/databases/mongo.js
+++ src/databases/mongo.js
@@ -3,19 +3,21 @@ const  { mongodb, mongodbsrv } = require('../config')
 require('dotenv').config()
 let connectionStringConfig = ""
 
-console.log(!process.env.APP_DEBUG);
+console.log(process.env.APP_DEBUG);
 
 /** 
  * Validamos si esta en desarollo o producción para conectarnos a la DB de la nube
  * @value true -> esta en desarrollo o local
  * @value false -> esta en producción
 */
-if(true){
-    connectionStringConfig = `mongodb://${mongodbsrv.user}:${mongodbsrv.password}@${mongodbsrv.host}:${mongodbsrv.port}`
-}else{
+if(process.env.APP_DEBUG === true){
+    console.log("Se cargo la conexion a la db local");
     connectionStringConfig = `mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`
+}else{
+    console.log("Se cargo la conexion a la db cloud");
+    connectionStringConfig = `mongodb://${mongodbsrv.user}:${mongodbsrv.password}@${mongodbsrv.host}:${mongodbsrv.port}`
 }
-console.log(connectionStringConfig);
+
 const connection = mongoose.connect(connectionStringConfig)
 .then(res =>{
     console.log('Conexion exitosa')
