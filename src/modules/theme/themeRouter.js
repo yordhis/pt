345e14fff75195a451b7c4a7dd10d96366b9683b
yordhis <yordhis.10@gmail.com>
2024-08-16 18:49:11 -0400

    Se agrego el endpoint tematicas a las rutas globales

--- src/modules/theme/themeRouter.js
+++ src/modules/theme/themeRouter.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const { all, filterById, register, update, destroy } = require('./themeController')
 const validateNameTheme = require('./middlewares/validateNameTheme')
+const validateUpdateNameTheme = require('./middlewares/validateUpdateNameTheme')
+const validatePermissionContentCategory = require('./middlewares/validatePermissionContentCategory')
 const router = express.Router()
 
 
 
 router.get( '/', all )
 router.get( '/:id', filterById )
-router.post( '/', validateNameTheme ,register )
-router.put( '/:id', validateNameTheme, update )
+router.post( '/', [ validateNameTheme, validatePermissionContentCategory ], register )
+router.put( '/:id', [ validateUpdateNameTheme, validatePermissionContentCategory ], update )
 router.delete( '/:id', destroy )
 
 module.exports = router
