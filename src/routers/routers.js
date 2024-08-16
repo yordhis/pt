d4361fe0c1274778ebd7ef38af76b0b83bc5b3dc
yordhis <yordhis.10@gmail.com>
2024-08-16 08:30:19 -0400

    Se añadio un archivo routers.js para manejar todas las rutas de la app

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const profileRouter = require('../modules/profile/profileRouter')
+const authRouter = require('../modules/auth/authRouter')
+const contentCategoryRouter = require('../modules/contentCategory/contentCategoryRouter')
 
-router.use('/profiles', profileRouter)
+router.use('/api/auth', authRouter)
+
+
+// eslint-disable-next-line no-unused-vars
+const verifyToken = require('../middlewares/verifyToken')
+router.use('/api/categories', contentCategoryRouter)
+router.use('/api/profiles', profileRouter)
 
 module.exports = router
