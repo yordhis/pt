15d362470dd48843f1c1c91b3fd69086e5082f4c
yordhis <yordhis.10@gmail.com>
2024-08-16 15:35:53 -0400

    Se agrego la ruta de themes en las rutas globales

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -3,13 +3,14 @@ const router = express.Router()
 const profileRouter = require('../modules/profile/profileRouter')
 const authRouter = require('../modules/auth/authRouter')
 const contentCategoryRouter = require('../modules/contentCategory/contentCategoryRouter')
+const themeRouter = require('../modules/theme/themeRouter')
 
 router.use('/api/auth', authRouter)
 
-
-// eslint-disable-next-line no-unused-vars
-const verifyToken = require('../middlewares/verifyToken')
+router.use('/api/themes', themeRouter)
 router.use('/api/categories', contentCategoryRouter)
 router.use('/api/profiles', profileRouter)
+
+// const verifyToken = require('../middlewares/verifyToken')
 
 module.exports = router
