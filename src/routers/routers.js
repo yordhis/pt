efb668b3021a8156b875c168f85f1c5003eeadd9
yordhis <yordhis.10@gmail.com>
2024-08-17 00:33:48 -0400

    Se agrego la ruta rols en las rutas globales ya esta habilitada

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -4,9 +4,11 @@ const profileRouter = require('../modules/profile/profileRouter')
 const authRouter = require('../modules/auth/authRouter')
 const contentCategoryRouter = require('../modules/contentCategory/contentCategoryRouter')
 const themeRouter = require('../modules/theme/themeRouter')
+const rolRouter = require('../modules/rol/rolRouter')
 
 router.use('/api/auth', authRouter)
 
+router.use('/api/rols', rolRouter)
 router.use('/api/themes', themeRouter)
 router.use('/api/categories', contentCategoryRouter)
 router.use('/api/profiles', profileRouter)
