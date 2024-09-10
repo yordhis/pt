3a8f32d704781b7f67f0dd4ee4afc2a089f4cbc6
yordhis <yordhis.10@gmail.com>
2024-09-10 18:28:41 -0400

    Se creo el archivo test de rol

--- __tests__/library.test.js
+++ __tests__/library.test.js
@@ -4,6 +4,12 @@ const request = require('supertest')
 const app = require('../src/app')
 
 
-describe('testing the multimedia content module', () => {
-    
+describe('Testing the multimedia content module', () => {
+    describe('Router [Get]/libreries', () => {
+        test('Should return the content without multimedia links', async () => {
+            const response = await request( app ).get('/api/libreries')
+
+            expect(response.statusCode).toEqual(200)
+        })
+    })
 })
