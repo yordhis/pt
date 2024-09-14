de919b2b5f11168c4a6fa4753ab6f72f2738f05d
yordhis <yordhis.10@gmail.com>
2024-09-13 22:54:37 -0400

    test libraries mokeado correctamente

--- __tests__/library.test.js
+++ __tests__/library.test.js
@@ -1,65 +1,107 @@
 /* eslint-disable no-undef */
-const request = require('supertest')
+// const request = require('supertest')
 
-const app = require('../src/app')
+// const app = require('../src/app')
 const LibraryService = require('../src/modules/library/libraryService')
+jest.mock('../src/modules/library/libraryService')
+const mockAll = jest.fn(( user ) => {
 
+        return [
+                {
+                    id:'65464654',
+                    theme:'ciencias',
+                    title: 'Fisica cuantica mock',
+                    description: 'Estudio de los mock cuanticos',
+                    links: user.rol == 'readertxt' ? null :{ image: ['urls'], video: ['urls'], txt: ['urls'], audio: ['urls'] },
+                    views: 100,
+                    buttons: user.rol == 'readertxt' ? null : { 
+                        get:{ action:'url-action', method:'GET' }, 
+                        put:{ action:'url-action', method:'PUT' },
+                        delete:{ action:'url-action', method:'DELETE' },
+                    },
+                    author: 'creador del contenido',
+                    credit: 'usuario creador',
+                }
+            ]
+        
+        
+    })
+const mockUpdate = jest.fn(() => {
+        return {
+            message: 'Resource update',
+            status: 200,
+        }
+    })
+
+const mockRegister = jest.fn((data) => {
+
+        if(data){
+            return {
+                message: 'Resource register',
+                status: 201,
+            }
+        }
+
+    })
+const mockDelete = jest.fn((id) => {
+
+        if(id){
+            return {
+                message: 'Resource delete',
+                status: 200,
+            }
+        }
+
+    })
+
+LibraryService.mockImplementation(() => {
+    return {
+        register: mockRegister,
+        update: mockUpdate,
+        all: mockAll,
+        destroy: mockDelete,
+    }
+})
   
-
-
-
 describe('Testing the multimedia content module', () => {
-    describe('Router [Get]/libreries', () => {
 
-        test('Should return status 200', async () => {
-            const response = await request( app ).get('/api/libraries')
-            expect(response.statusCode).toEqual(200)
-        })
-
-        test('Should return a object json witch the property data, status and message.', async () => {
-            const response = await request( app ).get('/api/libraries')
-            expect(response.body).toHaveProperty('data')
-            expect(response.body).toHaveProperty('status', 200)
-            expect(response.body).toHaveProperty('message', 'Ok')
-                
-            expect(response.statusCode).toEqual(200)
-        })
+    describe('Router [GET]/libreries', () => {
+        const users = [
+            { username: 'reader', email: 'reader@reader.com', password: '12345678', rol: 'reader', modules:['libraries'], permissions:['GET'],  profile:{} },
+            { username: 'creator', email: 'creator@creator.com', password: '12345678', rol: 'creator', modules:['libraries'], permissions:['GET', 'PUT'],  profile:{} },
+            { username: 'admin', email: 'admin@admin.com', password: '12345678', rol: 'admin', modules:['libraries'], permissions:['GET', 'POST', 'PUT', 'DELETE'],  profile:{} },
+            { username: 'anonimo', email: 'anonimo@anonimo.com', password: '12345678', rol: 'readertxt', modules:['libraries'], permissions:[],  profile:{} },
+        ]
 
         test('The data property is expected to be an array with objects', async () => {
-            const response = await request( app ).get('/api/libraries')
+            const library = new LibraryService()
+            const result = await library.all(users[3]) 
             
-            expect(response.body).toHaveProperty('data')
-            expect(response.body.data[0]).toHaveProperty('id')
-            expect(response.body.data[0]).toHaveProperty('theme')
-            expect(response.body.data[0]).toHaveProperty('title')
-            expect(response.body.data[0]).toHaveProperty('description')
-            expect(response.body.data[0]).toHaveProperty('links')
-            expect(response.body.data[0]).toHaveProperty('views')
-            expect(response.body.data[0]).toHaveProperty('buttons')
-            expect(response.body.data[0]).toHaveProperty('author')
-            expect(response.body.data[0]).toHaveProperty('credit')
-
-            expect(response.statusCode).toEqual(200)
+            expect(result[0]).toHaveProperty('id')
+            expect(result[0]).toHaveProperty('theme')
+            expect(result[0]).toHaveProperty('title')
+            expect(result[0]).toHaveProperty('description')
+            expect(result[0]).toHaveProperty('links')
+            expect(result[0]).toHaveProperty('views')
+            expect(result[0]).toHaveProperty('buttons')
+            expect(result[0]).toHaveProperty('author')
+            expect(result[0]).toHaveProperty('credit')
         })
 
         describe('Validating responses based on user role', () => {
-            const users = [
-                { username: 'reader', email: 'reader@reader.com', password: '12345678', rol: 'reader', modules:['libraries'], permissions:['GET']  },
-                { username: 'creator', email: 'creator@creator.com', password: '12345678', rol: 'creator', modules:['libraries'], permissions:['GET', 'PUT'] },
-                { username: 'admin', email: 'admin@admin.com', password: '12345678', rol: 'admin', modules:['libraries'], permissions:['GET', 'POST', 'PUT', 'DELETE'] },
-            ]
+            
 
             test('For unregistered users it should return resources without multimedia content', async () => {
-                const response = await request( app ).get('/api/libraries')
-                expect(response.body).toHaveProperty('data')
-                expect(response.body.data[0]).toHaveProperty('links', null)
-                expect(response.body.data[0]).toHaveProperty('buttons', null)
+                const library = new LibraryService()
+                const result = await library.all(users[3]) 
+                expect(result[0]).toHaveProperty('links', null)
+                expect(result[0]).toHaveProperty('buttons', null)
             })
     
             test('Should return the get buttons for the *Reader* role.', async () => {
         
-                const library = new LibraryService(users[0])
-                const result = await library.all() 
+                const library = new LibraryService()
+                const result = await library.all(users[0]) 
                 
                 expect(result[0]).toHaveProperty('buttons')
                 expect(result[0].buttons).toHaveProperty('get')
@@ -71,8 +113,8 @@ describe('Testing the multimedia content module', () => {
 
             test('Should return the get and put buttons for the *Creator* role.', async () => {
         
-                const library = new LibraryService(users[1])
-                const result = await library.all() 
+                const library = new LibraryService()
+                const result = await library.all(users[1]) 
                 
                 expect(result[0]).toHaveProperty('buttons')
                 expect(result[0].buttons).toHaveProperty('get')
@@ -86,8 +128,8 @@ describe('Testing the multimedia content module', () => {
 
             test('Should return the all buttons for the *Admin* role.', async () => {
         
-                const library = new LibraryService(users[2])
-                const result = await library.all() 
+                const library = new LibraryService()
+                const result = await library.all(users[2]) 
                 
                 expect(result[0]).toHaveProperty('buttons')
 
@@ -110,8 +152,8 @@ describe('Testing the multimedia content module', () => {
 
             test('Should return the all the content multimedia in tha property links.', async () => {
         
-                const library = new LibraryService(users[2])
-                const result = await library.all() 
+                const library = new LibraryService()
+                const result = await library.all(users[2]) 
                 
                 expect( result[0] ).toHaveProperty('links')
 
@@ -129,35 +171,73 @@ describe('Testing the multimedia content module', () => {
        
     })
 
-    describe('Router [Post]/libreries', () => {
+    describe('Router [POST]/libreries', () => {
         
-        jest.mock('../src/modules/library/libraryService')
-        const mockUpdate = jest
-            .fn(() => {
-                return {
-                    message: 'Resource register',
-                    status: 201,
-                }
-            })
-        const mockRegister = jest
-            .fn(() => {
-                return {
-                    message: 'Resource register',
-                    status: 201,
-                }
-            })
-    
-        LibraryService.mockImplementation(() => {
-            return {
-                register: mockRegister
-               
-            }
+       const libraryMock = {
+            theme:'ciencias',
+            title: 'Fisica cuantica mock 2',
+            description: 'Estudio de los mock cuanticos',
+            links: { image: ['urls'], video: ['urls'], txt: ['urls'], audio: ['urls'] },
+            views: 100,
+            buttons: { 
+                get:{ action:'url-action', method:'GET' }, 
+                put:{ action:'url-action', method:'PUT' },
+                delete:{ action:'url-action', method:'DELETE' },
+            },
+            author: 'creador del contenido',
+            credit: 'usuario creador',
+        }
+
+        test('Should return of status 201 and a message Resource register', async () => {
+            const library = new LibraryService()
+            const result = await library.register(libraryMock) 
+
+            expect(result).toEqual({ message: 'Resource register', status: 201 })
+
         })
-        LibraryService.mockImplementation(() => {
-            return {
-                update: mockUpdate
-            }
-        })
+       
 
     })
+
+    describe('Router [PUT]/libreries', () => {
+        
+       const libraryMock = {
+            theme:'ciencias',
+            title: 'Fisica cuantica mock 2',
+            description: 'Estudio de los mock cuanticos',
+            links: { image: ['urls'], video: ['urls'], txt: ['urls'], audio: ['urls'] },
+            views: 100,
+            buttons: { 
+                get:{ action:'url-action', method:'GET' }, 
+                put:{ action:'url-action', method:'PUT' },
+                delete:{ action:'url-action', method:'DELETE' },
+            },
+            author: 'creador del contenido',
+            credit: 'usuario creador',
+        }
+
+        test('Should return of status 200 and a message Resouce update', async () => {
+            const library = new LibraryService()
+            const result = await library.update(libraryMock) 
+
+            expect(result).toEqual({ message: 'Resource update', status: 200 })
+
+        })
+       
+
+    })
+
+    describe('Router [DELETE]/libreries', () => {
+
+        test('Should return of status 200 and a message Resouce delete', async () => {
+            const library = new LibraryService()
+            const result = await library.destroy('21') 
+
+            expect(result).toEqual({ message: 'Resource delete', status: 200 })
+
+        })
+       
+
+    })
+    
 })
